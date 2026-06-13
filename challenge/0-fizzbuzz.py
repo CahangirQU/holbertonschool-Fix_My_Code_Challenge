4b9905b21506ff269dbea8db60c627b687b8c013
CahangirQU <gence2017m@gmail.com>
2026-06-13 17:39:27 +0000

    Fix logic error and add execution permission

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,38 +1,27 @@
-#!/usr/bin/python3
-""" FizzBuzz
-"""
+#!/usr/bin/env python3
 import sys
 
-
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
     if n < 1:
         return
 
-    tmp_result = []
+    result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")   
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        # ƏN BİRİNCİ 15-ə bölünməni yoxlayırıq!
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+            result.append(str(i))
+    
+    print(" ".join(result))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
+        print("Missing argument")
         sys.exit(1)
 
     number = int(sys.argv[1])
